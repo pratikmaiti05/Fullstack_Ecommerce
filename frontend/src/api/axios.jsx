2aa618ed80d90813b4a3fd645873f7ad42af75dd
Pratik Maiti <maitipratik83@gmail.com>
2025-10-26 20:51:44 +0530

    Change axios base URL to production endpoint

--- frontend/src/api/axios.jsx
+++ frontend/src/api/axios.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const instance=axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://fullstack-ecommerce-backend-ozwz.onrender.com",
   withCredentials:true
 })
 export default instance
