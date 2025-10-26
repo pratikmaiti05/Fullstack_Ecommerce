a2e10bde9d756773d895b9560a9ada8c991125f2
Pratik Maiti <maitipratik83@gmail.com>
2025-10-26 21:04:47 +0530

    Update axios base URL for API requests

--- frontend/src/api/axios.jsx
+++ frontend/src/api/axios.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const instance=axios.create({
-  baseURL: "https://fullstack-ecommerce-backend-ozwz.onrender.com",
+  baseURL: "https://fullstack-ecommerce-backend-i8g8.onrender.com",
   withCredentials:true
 })
 export default instance
